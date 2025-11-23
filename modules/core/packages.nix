90faf71eafd0a51b8bb0fee50dab38bb87700b27
hirancph <hiranchandph@gmail.com>
2025-11-23 21:57:31 +0530

    added aider

--- modules/core/packages.nix
+++ modules/core/packages.nix
@@ -33,6 +33,7 @@
   environment.systemPackages = with pkgs; [
     # Hyprland systeminfo QT  (Optional)
     #inputs.hyprsysteminfo.packages.${pkgs.system}.default
+    aider-chat-full
     android-tools
     amfora # Fancy Terminal Browser For Gemini Protocol
     appimage-run # Needed For AppImage Support
