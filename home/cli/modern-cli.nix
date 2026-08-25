b20509c7579bb5da83d1c11a190a82f3d6be4caa
hirancph <hiranchandph@gmail.com>
2026-08-25 20:48:22 +0530

    feat(home): add modern CLI tools configuration

---
+++ home/cli/modern-cli.nix
@@ -0,0 +1,20 @@
+{pkgs, ...}: {
+  programs.fish = {
+    enable = true;
+
+    shellAliases = {
+      cat = "bat";
+      ls = "eza";
+      la = "eza -lah";
+      ll = "eza -lh --no-user --long";
+      lt = "eza --tree --level=2";
+      tree = "eza --tree";
+    };
+  };
+  home.packages = with pkgs; [
+    eza
+    bat
+    fd
+    rigrep
+  ];
+}
