a88ffc4671c1b85942ed3d04391e298f15a9d855
hirancph <hiranchandph@gmail.com>
2026-08-26 20:21:50 +0530

    feat: add noctalia

--- flake.nix
+++ flake.nix
@@ -4,12 +4,18 @@
   inputs = {
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Home manager
     home-manager = {
       url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    noctalia = {
+      url = "github:noctalia-dev/noctalia";
+      inputs.nixpkgs.follows = "nixpkgs"; # this line is optional, prevents downloading two versions of nixpkgs but disables cache
     };
 
     nix-flatpak.url = "github:gmodena/nix-flatpak";
