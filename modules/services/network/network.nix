ba3e00c3b66fd5ae87762c09f34d283cf47be9fb
hirancph <hiranchandph@gmail.com>
2026-08-27 12:20:08 +0530

    feat(network): change nameservers

--- modules/services/network/network.nix
+++ modules/services/network/network.nix
@@ -1,6 +1,10 @@
 {pkgs, ...}: {
   networking = {
-    nameservers = ["9.9.9.9#dns.quad9.net"];
+    nameservers = [
+      "8.8.8.8"
+      "8.8.4.4"
+      "1.1.1.1"
+    ];
 
     networkmanager = {
       enable = true;
