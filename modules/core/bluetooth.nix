be34f58935b4dcb5c1b4ac220324c71818166fbb
hirancph <hiranchandph@gmail.com>
2026-08-28 23:13:24 +0530

    refactor(core): remove blueman

--- modules/core/bluetooth.nix
+++ modules/core/bluetooth.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}: {
-  environment.systemPackages = with pkgs; [blueman];
+  #environment.systemPackages = with pkgs; [blueman];
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
